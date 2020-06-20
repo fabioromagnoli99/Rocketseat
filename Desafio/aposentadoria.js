9189ad7f097cb2a8c45eba1e12944fc6eba33b2e
Fabio Romagnoli <32691245+fabioromagnoli99@users.noreply.github.com>
2020-06-20 08:13:28 +0100

    Desafio 2

--- Desafio/aposentadoria.js
+++ Desafio/aposentadoria.js
@@ -1,3 +1,8 @@
+// O tempo de contribuição mínimo para homens é de 35 anos e, para mulheres, 30 anos;
+// Utilizando a regra 85-95, a soma da idade com o tempo de contribuição do homem precisa ser de no mínimo 95 anos, 
+// enquanto a mulher precisa ter no mínimo 85 anos na soma;
+
+
 const nome = "Fabio"
 const sexo = "M"
 const idade = 50
